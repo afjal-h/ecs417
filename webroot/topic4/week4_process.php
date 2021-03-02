f72516dda5614f5020e4a1a29f92af553b23d926
afjal-h <78381542+afjal-h@users.noreply.github.com>
2021-03-02 11:28:57 +0000

    Add files via upload

---
+++ webroot/topic4/week4_process.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+   <meta charset="utf-8">
+   <title>Week 4 - Form Results</title>
+   <link rel="stylesheet" href="reset.css"/>
+   <link rel="stylesheet" href="process.css" />
+</head>
+<body>
+   <div class="box">
+     <h2>Get Method Data</h2>
+     <p>No GET variables found.</p>   </div>
+   <div class="box">
+     <h2>Post Method Data</h2>
+     <p>No POST variables found.</p>   </div>
+</body>
+</html>
