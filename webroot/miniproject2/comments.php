a83c4170f74bd751317cf28b83b7440a612c46a6
afjal-h <78381542+afjal-h@users.noreply.github.com>
2021-04-07 22:14:49 +0100

    Add files via upload

--- webroot/miniproject2/comments.php
+++ webroot/miniproject2/comments.php
@@ -72,7 +72,7 @@ session_start();
                             <h3 class="card-title"><?php echo $q['name'];?></h3>
                             <p class="card-text"><?php echo $q['content'];?></p>
                             <?php
-                            if (isset($_SESSION['Admin'])){
+                            if ($_SESSION['Admin']==TRUE){
                               echo '<a href="deleteComment.php? id='.$q["id"].' name="delete" class="btn btn-light">Delete </a>';
 
                             }
