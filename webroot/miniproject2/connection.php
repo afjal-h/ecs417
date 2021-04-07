572f7f161af356e8465f9f4d947184a332f47d4e
afjal-h <78381542+afjal-h@users.noreply.github.com>
2021-04-07 21:52:34 +0100

    Add files via upload

--- webroot/miniproject2/connection.php
+++ webroot/miniproject2/connection.php
@@ -1,5 +1,13 @@
 <?php
-$connect=mysqli_connect('localhost','root', '','users');
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER");
+$dbpwd = getenv("DATABASE_PASSWORD");
+$dbname = getenv("DATABASE_NAME");
+// Creates connection
+$connect = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
+// Checks connection
+
 
 if(!$connect){
   die('Connection failed'.mysqli_error());
